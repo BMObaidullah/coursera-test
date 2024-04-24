17e8c31f51819bff6ed9dfa4b460396d63e5d3b7
BMObaidullah <m4obaidullah@gmail.com>
2024-04-24 16:16:30 +0600

    did some practice with JS

--- Course-1/HTML CSS JS/js/script.js
+++ Course-1/HTML CSS JS/js/script.js
@@ -1,3 +1,17 @@
-for (var i = 1; i < 11; i++) {
-  console.log(i);
+function sayHello() {
+  this.textContent = "said it!";
+  var title = document.getElementById("heading").textContent;
+
+  title += " and it's very big";
+
+  if (document.getElementById("name").value === "student") {
+    document.getElementById("heading").textContent = title;
+    document.getElementById("content").textContent =
+      "Hello " + document.getElementById("name").value + "!";
+  } else {
+    document.getElementById("content").textContent =
+      "Hello " + document.getElementById("name").value + "!";
+  }
 }
+
+document.querySelector("button").onclick = sayHello;
